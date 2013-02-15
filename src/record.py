a614c3e45670d063e65f1d281606e9eb172284f7
Hermes Lyu <a108210@gmail.com>
2013-02-15 14:35:04 +0800

    Realtime play sound done

--- src/record.py
+++ src/record.py
@@ -1,49 +1,76 @@
 #!/usr/bin/python
 import subprocess
 import os 
+import sys 
 import settings 
 import music21 
+from collections import deque
 #infname = './test.mp3';
 #outfname = './test.txt';
 def playStream(s):
    sp = music21.midi.realtime.StreamPlayer(s)
    sp.play()
-def record(recLogFilename):
+
+def playNote(n):
+   s = music21.stream.Stream()
+   s.append(n)
+   playStream(s)
+
+def record(score, recLogFilename):
    period = 1; # in ms
    print("[INFO] Remember to turn on SHMConfig for the touchpad. See README for detail.")
-   cmd = ['synclient',  '-m' , str(period)];
+   cmd = ['synclient',  '-m' , str(period)]
    settings.printDebug(cmd)
-   key = 'r';
-   while (key != ''):
-      with open(recLogFilename, 'w') as outfile:
-         p = subprocess.Popen(cmd, stdout = outfile);
-      key = raw_input('Press ENTER to continue, "r"-Enter to restart.');
-      settings.printDebug(key)
-      p.kill();
+   p = subprocess.Popen(cmd, stdout = subprocess.PIPE)
+   print('[INFO] Press Ctrl+c to stop recording the current phrase.')
+   noteIter = iter(score)
+   queue = deque() #enqueue from right, dequeue from left
+   perf = []
+   prevParsedLine = {'time': 0, 'pressure': 0, 'fingerCount': 0}
+   while True:
+      try:
+         line  = p.stdout.readline()
+         settings.printDebug(line)
+         sys.stdout.flush()
 
-#def annotate(): #deprecated
-#   #outPath = '../records/';
-#   period = 1; # in ms
-#   cmd = ['synclient',  '-m' , str(period)];
-##cmd = ['synclient']; 
-#   if DEBUG: print(cmd);
-#   fileNo = 0;
-#   key = '';
-#   while (key != 'q'):
-#
-#      #outfname = outPath + 'record_' + str(fileNo) + '.txt';
-#      with open(outfname, 'w') as outfile:
-#         p = subprocess.Popen(cmd, stdout = outfile);
-#      key = raw_input('Press ENTER to end current recording and continue. Press q-Enter to quit.');
-#      if DEBUG: print(key);
-#      p.kill();
-#      fileNo+=1;
-#      #if (key == 'q'):
-      #   os.remove(outfname);
-         
+         #if len(line.split()) == 17: 
+            #(time, x, y, z, f, w, l, r, u, d, m, multi, gl, gm, gr, gdx, gdy) = line.split(); 
+         if len(line.split()) != 12:
+            print('[WARN] Unknown line format. Skipped.');
+            continue;
 
-#if (key == ')
-#subprocess.call(['mplayer', infname]);
-#   process.process(outPath)
-#if __name__ == '__main__':
-#   annotate();
+         (time, x, y, z, f, w, l, r, u, d, m, multi) = line.split();
+         parsedLine = {'time': float(time), 'pressure': int(z), 'fingerCount': int(f)}
+         #recLog.append({'time': float(time), 'pressure': int(z), 'fingerCount': int(f)})
+
+         if parsedLine['fingerCount'] > prevParsedLine['fingerCount']:
+            #Note on event
+            try:
+               note = noteIter.next()
+            except StopIteration:
+               print('[ERROR]: Your recording is longer than the score. Stopped')
+               break
+               #raise Exception('[ERROR]: Your recording is longer than the score. Stopped')
+            playNote(note)
+            queue.append({'onset':parsedLine['time'], 
+                          'velocity': parsedLine['pressure'],
+                          'pitch': note.pitch,
+                          'duration': None
+                          })
+            #noteon
+            settings.printDebug(queue)
+         elif parsedLine['fingerCount'] < prevParsedLine['fingerCount']:
+            #note off event
+            note = queue.popleft()
+            settings.printDebug(queue)
+            note['duration'] = parsedLine['time'] - note['onset'] 
+            perf.append(note)
+         prevParsedLine = parsedLine
+
+      except KeyboardInterrupt:
+         print('[INFO] Current phrase is cut by user')
+         break 
+
+   p.kill()
+   return perf
+
