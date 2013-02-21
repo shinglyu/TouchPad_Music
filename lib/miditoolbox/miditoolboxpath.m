600408a602f15d33ed8c6c81afa6bdd08b898e39
Hermes Lyu <a108210@gmail.com>
2013-02-22 00:17:30 +0800

    Started split.py; Cleanup

--- lib/miditoolbox/miditoolboxpath.m
+++
@@ -1,6 +0,0 @@
-function p = miditoolboxpath()
-% p = miditoolboxpath()  Return path to miditoolbox directory
-% 2004-08-13 dpwe@ee.columbia.edu
-
-cmd = 'miditoolboxpath';
-p = fileparts(which(cmd));
--- src/split.py
+++ src/split.py
@@ -1,2 +1,22 @@
+import simplejson
+import music21
 
-def split()
+def findScoreInfo(score, infoClass): #infoClass can be TImeSignature or other
+
+def split(splitRecFilename, origScoreFilename):
+   with open(splitRecFilename, 'r') as f:
+      splitRec = simplejson.load(f)
+   origScore = music21.converter.parse(origScoreFilename)
+
+   for offsetSegment in splitRec:
+      minOffset = min(offsetSegment)
+      maxOffset = max(offsetSegment)
+      scoreSegment = (origScore.flat.notes
+                      .getElementsByOffset(minOffset, maxOffset))
+      #get TimeSignature and key
+      #prepent TimeSig and Key to scoreSegment, not the init offset 
+      #save the scoreSegment to xml
+
+   #refactor this to recursion or something because we need counter
+      
+      
